3fb546459fb4b41c59a000b521e87563af42f19d
Preet Panchal <preet2020@icloud.com>
2023-02-20 11:03:07 -0500

    main

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -12,6 +12,7 @@ import BlogEditor from "../../components/BlogEditor";
 import { useRouter } from "next/router";
 import Cursor from "../../components/Cursor";
 import data from "../../data/portfolio.json";
+import Image from "next/image";
 
 const BlogPost = ({ post }) => {
   const [showEditor, setShowEditor] = useState(false);
@@ -39,10 +40,12 @@ const BlogPost = ({ post }) => {
         <Header isBlog={true} />
         <div className="mt-10 flex flex-col">
           <Image
-            className="w-full h-96 rounded-lg shadow-lg object-cover"
             src={post.image}
             alt={post.title}
-          ></Image>
+            width={500}
+            height={500}
+            layout="responsive"
+          />
           <h1
             ref={textOne}
             className="mt-10 text-4xl mob:text-2xl laptop:text-6xl text-bold"
