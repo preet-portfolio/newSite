98d3abc86d7689bb52ceb76d871cf27443d39323
Preet Panchal <preet2020@icloud.com>
2023-02-24 00:53:36 -0500

    Update index.js

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -3,18 +3,24 @@ import React from "react";
 
 const WorkCard = ({ img, name, description, onClick }) => {
   return (
-    <div className="overflow-hidden rounded-lg p-2 md:p-4 first:ml-0 link">
-      <div className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 ">
+    <div
+      className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
+      onClick={onClick}
+    >
+      <div
+        className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
+        style={{}}
+      >
         <Image
           alt={name}
-          className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
+          className="h-full w-full object-fill hover:scale-110 transition-all ease-out duration-300"
           src={img}
-        />
+        ></Image>
       </div>
-      <h1 className="mt-5 text-3xl font-medium md:mt-8">
+      <h1 className="mt-5 text-3xl font-medium">
         {name ? name : "Project Name"}
       </h1>
-      <h2 className="text-xl opacity-50 md:text-lg">
+      <h2 className="text-xl opacity-50">
         {description ? description : "Description"}
       </h2>
     </div>
